5131d37bfdf40b0fe09d9622b56e9bfcd5d81c3e
Jenny Svensson <hello@jennysvensson.com>
2021-06-27 22:40:06 +0200

    Conditional headline

--- src/components/todoList/TodoList.js
+++ src/components/todoList/TodoList.js
@@ -6,9 +6,11 @@ import TodoItem from './TodoItem';
 const TodoList = ({ todoList, doneItem, deleteItem }) => (
   <section className='app__section todo-list'>
     <h2 className='todo-list__heading'>To Do</h2>
-    <h2 className='todo-list__heading todo-list__heading-done'>Done <span className='todo-list__heading-icon'>
-      <FaCheckCircle />
-    </span></h2>
+    {todoList.length > 0
+      ? <h2 className='todo-list__heading todo-list__heading-done'>Done <span className='todo-list__heading-icon'>
+        <FaCheckCircle />
+      </span></h2>
+      : <p>Your todo-list is empty. Please add new tasks above.</p>}
     {todoList.map(item => (
       <TodoItem key={item.id} item={item} doneItem={doneItem} deleteItem={deleteItem} />
     ))}
